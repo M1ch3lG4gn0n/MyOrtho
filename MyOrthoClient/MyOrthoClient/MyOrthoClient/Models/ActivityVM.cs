1d2b150fd120190e636d67037bb5d864974a2a6d
DESKTOP-GDF5VKV\Gabriel <goodie_fire@hotmail.com>
2017-10-01 14:25:55 -0400

    activity properties

--- MyOrthoClient/MyOrthoClient/MyOrthoClient/Models/ActivityVM.cs
+++ MyOrthoClient/MyOrthoClient/MyOrthoClient/Models/ActivityVM.cs
@@ -8,5 +8,15 @@ namespace MyOrthoClient.Controllers
 {
     class ActivityVM
     {
+        private int Pitch { get; set; }
+        private int Intensity { get; set; }
+        private string Example_wav_path { get; set; }
+        private int F0_exact { get; set; }
+        private int F0_stable { get; set; }
+        private int Intensite_stable { get; set; }
+        private int Courbe_f0_exacte { get; set; }
+        private int Duree_exacte { get; set; }
+        private int Jitter { get; set; }
+        private string Name { get; set; }
     }
 }
--- MyOrthoClient/MyOrthoClient/MyOrthoClient/Views/MainWindow.xaml.cs
+++ MyOrthoClient/MyOrthoClient/MyOrthoClient/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyOrthoClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,12 @@ namespace MyOrthoClient
     {
         public MainWindow()
         {
+            
             InitializeComponent();
+
+            ListVM activityListInstance = new ListVM();
+            this.DataContext = activityListInstance;
+            
         }
     }
 }
