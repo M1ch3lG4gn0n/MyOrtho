dca2fee2faa87e7f4a0cd6fc616fce0ba3ac3ce3
M1ch3lG4gn0n <cplgagnonm@hotmail.com>
2017-12-12 21:52:40 -0500

    XmlDoc

---
+++ MyOrthoOrtho/MyOrthoOrtho/Controllers/XmlHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyOrthoOrtho.Controllers
+{
+    public class XmlHelper
+    {
+        private XmlDocument document = new XmlDocument();
+        private XmlElement root;
+
+        public XmlHelper(bool withHeader)
+        {
+            if (withHeader)
+            {
+                XmlDeclaration xmlDeclaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);
+                root = document.DocumentElement;
+                document.InsertBefore(xmlDeclaration, root);
+            }
+        }
+
+        public XmlElement AddToRoot(string name, string value)
+        {
+            XmlElement node = document.CreateElement(string.Empty, name, string.Empty);
+            if (!string.IsNullOrEmpty(value))
+            {
+                XmlText textNode = document.CreateTextNode(value);
+                node.AppendChild(textNode);
+            }
+            document.AppendChild(node);
+            return node;
+        }
+
+        public XmlElement AppendToNode(XmlElement node, string name, string value)
+        {
+            XmlElement childNode = document.CreateElement(string.Empty, name, string.Empty);
+            if (!string.IsNullOrEmpty(value))
+            {
+                XmlText textValue = document.CreateTextNode(value);
+                childNode.AppendChild(textValue);
+            }
+            node.AppendChild(childNode);
+            return childNode;
+        }
+
+        public void Save(string path)
+        {
+            document.Save(path);
+        }
+        
+    }
+}
